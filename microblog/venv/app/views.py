27cd5d0947d435f6dc02cb03e94492bbaf02bd00
cburk <cjb6@rice.edu>
2015-02-28 21:34:10 -0600

    Should be virtual environment and skeleton

---
+++ microblog/venv/app/views.py
@@ -0,0 +1,35 @@
+from flask import render_template
+from app import app
+from .forms import LoginForm
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html',
+							title='Sign In',
+							form=form)
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {'nickname': 'Miguel'} #fake user
+	posts = [
+		{ 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+	]
+	
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							#user={'nickname':''},
+							posts=posts)
+	
+@app.route('/seven')
+def ret_seven():
+	return "7?"
